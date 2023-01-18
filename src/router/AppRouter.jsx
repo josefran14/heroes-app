629725882fb24baefd8afa5bdbec246d83ae553f
josefran14 <fransaavedra21@gmail.com>
2023-01-18 18:11:50 -0400

    Se añadio un nav y se coloco la clase de la ruta activa implicitamente

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,10 +2,13 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import { LoginPage } from '../auth/pages/LoginPage'
 import { DcPage } from '../heroes/pages/DcPage'
 import { MarvelPage } from '../heroes/pages/MarvelPage'
+import { Navbar } from '../ui/components/NavBar'
 
 export const AppRouter = () => {
   return (
    <>
+     <Navbar/>
+
      <Routes>
         <Route path='marvel' element={<MarvelPage/>}></Route>
         <Route path='dc' element={<DcPage/>}></Route>
