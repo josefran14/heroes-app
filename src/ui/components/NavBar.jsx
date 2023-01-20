e682168194c9b5a14e2ba8c4aa1b0ddf44f46318
josefran14 <fransaavedra21@gmail.com>
2023-01-20 15:17:45 -0400

    En la parte del logout del nav se establecio un evento onClick que al presionarlo nos va a dirigir a la parte del login, y dentro del login se creo un boton que al presionarlo nos regresa a la aplicacion, se utilizo el hook useNavigate para establecer las rutas y con el replace evitamos que podamos regresar al historial anterior

--- src/ui/components/NavBar.jsx
+++ src/ui/components/NavBar.jsx
@@ -1,6 +1,15 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
+
+   const navigate = useNavigate()
+
+   const onLogout = () =>{
+     navigate('/login', {
+      replace: true
+     })
+  }
+
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
       <Link className="navbar-brand" to="/">
@@ -27,14 +36,14 @@ export const Navbar = () => {
             DC
           </NavLink>
 
-          <NavLink
+          {/* <NavLink
             className={({ isActive }) =>
             `nav-item nav-link ${isActive ? "active" : ""}`
         }
             to="/search"
             >
             Search
-          </NavLink>
+          </NavLink> */}
 
         </div>
       </div>
@@ -43,7 +52,7 @@ export const Navbar = () => {
         <ul className="navbar-nav ml-auto">
           <span className="nav-item nav-link text-info">Francisco</span>
 
-          <button className="nav-item nav-link btn">Logout</button>
+          <button onClick={onLogout} className="nav-item nav-link btn">Logout</button>
         </ul>
       </div>
     </nav>
