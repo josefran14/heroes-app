0e45322c9d65dec8a1300d8b2911fc195a776a16
josefran14 <fransaavedra21@gmail.com>
2023-01-24 14:26:14 -0400

    Se diseño la pantalla de busqueda en el componente SearchPage

--- src/ui/components/NavBar.jsx
+++ src/ui/components/NavBar.jsx
@@ -36,14 +36,14 @@ export const Navbar = () => {
             DC
           </NavLink>
 
-          {/* <NavLink
+           <NavLink
             className={({ isActive }) =>
             `nav-item nav-link ${isActive ? "active" : ""}`
         }
             to="/search"
             >
             Search
-          </NavLink> */}
+          </NavLink> 
 
         </div>
       </div>
